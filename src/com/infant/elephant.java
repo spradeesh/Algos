e0efa50f51ef9aeb61f35b30e67b121efed87f9a
Palanisamy <A052634@deutscheleasing.de>
2020-11-09 23:28:15 +0100

    Initial commit

---
+++ src/com/infant/elephant.java
@@ -0,0 +1,14 @@
+package com.infant;
+
+public class elephant {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World 3!");
+        int n = 5;
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Hello World ---!");
+        }
+    }
+
+}
